1d862956404d0822771094a6d6a32a68e847dc9f
root <root@AumrudhLalKumarTJ.localdomain>
2020-11-15 16:32:18 +0530

    ex7

--- even/ex7/client.c
+++ even/ex7/client.c
@@ -1,65 +1,254 @@
-#include<stdio.h>
-#include<netdb.h>
-#include<arpa/inet.h>
-#include<netinet/in.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include<time.h>
+#define cls() printf("\n")
 
-int main(){
-        int sd,c,len;
-        int msg,ack,size,nack=1;
-        struct sockaddr_in server,client;
-        server.sin_family=AF_INET;
-        printf("Enter the port no : ");
-        int portno;
-        scanf("%d",&portno);
-        server.sin_port=htons(portno);
-        server.sin_addr.s_addr=htonl(INADDR_ANY);
+// structure definition for accepting the packets.
 
-        //socket creation
+struct frame
 
-        sd=socket(AF_INET,SOCK_STREAM,0);
-        if(sd==-1){
-                printf("Socker=t creation failed\n");
-                exit(0);
-        }
-        else{
-                printf("Socket Created\n");
-        }
+{
 
-        //connection establishment
+  int packet[40];
+};
 
-        len=sizeof(server);
-        c=connect(sd,(const struct sockaddr*)&server,len);
-        if(c==-1){
-                printf("Connection failed\n");
-                exit(0);
-        }
-        else{
-                printf("Connection established\n");
+// structure definition for constructing the acknowledgement frame
+
+struct ack
+
+{
+
+  int acknowledge[40];
+};
+
+int main()
+
+{
+
+  int clientsocket;
+
+  struct sockaddr_in serveraddr;
+
+  socklen_t len;
+
+  struct hostent *server;
+
+  struct frame f1;
+
+  int windowsize, totalpackets, totalframes, i = 0, j = 0, framesreceived = 0,
+                                             k, l, m, repacket[40];
+
+  struct ack acknowledgement;
+
+  char req[50];
+
+  char name[20] = {0}; // in case of single character input
+  fd_set input_set;
+  struct timeval  timeout;
+  int ready_for_reading = 0;
+
+  clientsocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+  bzero((char *)&serveraddr, sizeof(serveraddr));
+
+  serveraddr.sin_family = AF_INET;
+
+  serveraddr.sin_port = htons(5018);
+
+  server = gethostbyname("127.0.0.1");
+
+  bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr,
+        sizeof(server->h_addr));
+
+  // establishing the connection.
+
+  printf("\nSending request to the client.\n");
+
+  sendto(clientsocket, "HI I AM CLIENT.", sizeof("HI I AM CLIENT."), 0,
+         (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+
+  printf("\nWaiting for reply.\n");
+
+  recvfrom(clientsocket, req, sizeof(req), 0, (struct sockaddr *)&serveraddr,
+           &len);
+
+  printf("\nThe server has send:\t%s\n", req);
+
+  // accepting window size from the user.
+
+  printf("\nEnter the window size:\t");
+
+  scanf("%d", &windowsize);
+
+  // sending the window size.
+
+  printf("\n\nSending the window size.\n");
+
+  sendto(clientsocket, (char *)&windowsize, sizeof(windowsize), 0,
+         (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+
+  cls();
+
+  // collecting details from server.
+
+  printf("\nWaiting for the server response.\n");
+
+  recvfrom(clientsocket, (char *)&totalpackets, sizeof(totalpackets), 0,
+           (struct sockaddr *)&serveraddr, &len);
+
+  printf("\nThe total packets are:\t%d\n", totalpackets);
+
+  sendto(clientsocket, "RECEIVED.", sizeof("RECEIVED."), 0,
+         (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+
+  recvfrom(clientsocket, (char *)&totalframes, sizeof(totalframes), 0,
+           (struct sockaddr *)&serveraddr, &len);
+
+  printf("\nThe total frames/windows are:\t%d\n", totalframes);
+
+  sendto(clientsocket, "RECEIVED.", sizeof("RECEIVED."), 0,
+         (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+
+  // starting the process.
+
+  printf("\nStarting the process of receiving.\n");
+
+  j = 0;
+
+  l = 0;
+
+  while (l < totalpackets)
+
+  { // initialising the receive buffer.
+
+    printf("\nInitialising the receive buffer.\n");
+
+    printf("\nThe expected frame is %d with packets:  ", framesreceived);
+
+    for (m = 0; m < j; m++)
+
+    { // readjusting for packets with negative acknowledgement.
+
+      printf("%d  ", repacket[m]);
+    }
+
+    while (j < windowsize && i < totalpackets)
+
+    {
+
+      printf("%d  ", i);
+
+      i++;
+
+      j++;
+    }
+
+    printf("\n\nWaiting for the frame.\n");
+
+    // accepting the frame.
+
+label:  recvfrom(clientsocket, (char *)&f1, sizeof(f1), 0,
+             (struct sockaddr *)&serveraddr, &len);
+
+    printf("\nReceived frame %d\n\nEnter -1 to send negative acknowledgement "
+           "for the following packets.\n",
+           framesreceived);
+
+    // constructing the acknowledgement frame.
+
+    //printf("1-no ack(after 10 sec type )\n2-ack..enter choice : ");
+    //int choice;
+    //scanf("%d",&choice);
+    //if( choice == 1 ){
+    timeout.tv_sec = 5;
+          timeout.tv_usec = 0;
+	        printf("Enter name : \n");
+		      FD_SET(0, &input_set);
+		            ready_for_reading = select(1, &input_set, NULL, NULL, &timeout);
+			          if (ready_for_reading) {
+					          scanf("%s" , name);
+						          }
+				        else {
+						          printf(" 5 Seconds are over - no data input \n");
+    char msg[10] = "-2";
+	              sendto(clientsocket,msg, sizeof(msg), 0,
+				                 (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+				          goto label;
+    //}
+
+     j = 0;
+
+         m = 0;
+
+	     k = l;
+    while (m < windowsize && k < totalpackets)
+
+    {
+
+      printf("\nPacket: %d\n", f1.packet[m]);
+
+      // accepting acknowledgement from the user.
+     /* timeout.tv_sec = 5;
+      timeout.tv_usec = 0;
+      printf("Enter name : \n");
+      FD_SET(0, &input_set);
+      ready_for_reading = select(1, &input_set, NULL, NULL, &timeout);
+      if (ready_for_reading) {
+        scanf("%s" , name);
 	}
-	while(1){
-		read(sd,&msg,sizeof(int));
-		size++;
-		printf("The data is : %d",msg);
-		printf("1-No ACK\n2-Normal ACK\n3-NACK\nEnter you choice : ");
-		scanf("%d",&ack);
-		switch(ack){
-			case 1:
-			case 2:
-				ack=size;
-				write(sd,&ack,sizeof(int));
-				break;
-			case 3:
-				if(nack==1)
-					write(sd,&nack,sizeof(int));
-				else{
-					nack++;
-					write(sd,&nack,sizeof(int));
-				}
-				size--;
-				break;
-		}
+      else {
+          printf(" 5 Seconds are over - no data input \n");
+          acknowledgement.acknowledge[m] = -2;
+          sendto(clientsocket, (char *)&acknowledgement, sizeof(acknowledgement), 0,
+           (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+	  goto label;
+	}
+	*/
+	scanf("%d", &acknowledgement.acknowledge[m]);
+	//sprintf(acknowledgement.acknowledge[m],"%s",name);
+	//acknowledgement.acknowledge[m]=atoi(name);
+      if (acknowledgement.acknowledge[m] == -1)
+
+      {
+
+        repacket[j] = f1.packet[m];
+
+        j++;
+
+      }
+
+      else
+
+      {
+
+        l++;
+      }
+
+      m++;
+
+      k++;
+    }
+
+    framesreceived++;
+
+    // sending acknowledgement to the server.
+
+    sendto(clientsocket, (char *)&acknowledgement, sizeof(acknowledgement), 0,
+           (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+
+    cls();
+  }
+
+  printf("\nAll frames received successfully.\n\nClosing connection with the "
+         "server.\n");
+
+  close(clientsocket);
+}
